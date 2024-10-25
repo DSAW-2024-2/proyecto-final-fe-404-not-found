14aa8cd98902e721a24045a77e91b868fc13c9be
Mao Suarez <maosuarezbarrer@gmail.com>
2024-10-25 00:39:46 -0500

    Correccion de vista

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/proyecto-final-fe-404-not-found/', // Cambia esto si es necesario
-	build: {
-		outDir: 'dist', // El directorio donde se generará el build
-		sourcemap: true, // Genera un archivo sourcemap
-	},
 });
