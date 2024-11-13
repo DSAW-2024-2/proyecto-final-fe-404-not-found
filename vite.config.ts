978ae7b5e5ec7baed1f84a2d2151654f6c89ba33
Mao Suarez <maosuarezbarrer@gmail.com>
2024-11-13 08:51:01 -0500

    Intentoooooo ya me canseee

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://149.130.182.245:3001/api-wheels/v1', // Cambia esta URL por la de tu backend
-				changeOrigin: true,
-				secure: false, // Permite usar HTTP sin errores de certificado
-			},
-		},
-	},
 });
